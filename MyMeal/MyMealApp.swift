1fb0f754909dc550022bb073d32f04613e637a7b
divya-prav <divya.praveen.5215@gmail.com>
2024-02-05 12:12:12 -0600

    Initial Commit

---
+++ MyMeal/MyMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMealApp.swift
+//  MyMeal
+//
+//  Created by Divya Praveen on 2/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MyMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
