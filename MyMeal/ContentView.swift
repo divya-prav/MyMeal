ec2d9ac40250d487c85ceec826ad59d4b4d5b369
divya-prav <divya.praveen.5215@gmail.com>
2024-02-06 14:43:21 -0600

    List of Dessert updated

--- MyMeal/ContentView.swift
+++ MyMeal/ContentView.swift
@@ -7,18 +7,132 @@
 
 import SwiftUI
 
+
+class ViewModel: ObservableObject{
+    
+    @Published var recipes: [DessertMenu] = []
+    func fetch() {
+        guard let url = URL(string:
+            "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert")else{
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url){
+            data,_, error in
+            guard let data = data, error == nil else{
+                return
+            }
+            
+            do{
+                let responseData = try JSONDecoder().decode(ResponseData.self,from:
+                    data)
+                DispatchQueue.main.async {
+                    self.recipes = responseData.meals
+                }
+            }
+            catch{
+                print(error)
+            }
+        }
+        task.resume()
+    }
+}
+
 struct ContentView: View {
+   
+    @StateObject var viewModel = ViewModel()
+    var body: some View {
+        NavigationView {
+            List{
+                ForEach(viewModel.recipes,id: \.self){recipe in
+                    HStack {
+                        NavigationLink(destination : RecipeDetailView(recipe:recipe)){
+                            URLImage(urlString: recipe.strMealThumb)
+                                .frame(width: 100,height:100)
+                                .background(Color.gray)
+                            Text(recipe.strMeal)
+                                .bold()
+                            
+                        }
+                       
+                    }
+                    .padding(10)
+                }
+                
+            }
+            .navigationTitle("Dessert")
+            .onAppear{
+                viewModel.fetch()
+            }
+        }
+       
+    }
+   
+}
+
+struct URLImage : View {
+    let urlString : String
+    
+    @State var data: Data?
+    
+    var body: some View{
+        if let data = data,let uiimage = UIImage(data:data) {
+            Image(uiImage: uiimage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 100,height: 100)
+                .background(Color.gray)
+            
+        }else{
+            Image(systemName: "Dessert")
+                .frame(width: 100,height: 100)
+                .background(Color.gray)
+                .onAppear(){
+                    fetchData()
+                }
+        }
+    }
+    
+    private func fetchData(){
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url){data, _ , _ in self.data = data
+        }
+        task.resume()
+    }
+}
+
+struct RecipeDetailView: View {
+    let recipe: DessertMenu
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(recipe.strMeal)
+            // Add more details as needed
         }
-        .padding()
+        .navigationTitle(recipe.strMeal)
     }
+}
+
+struct ResponseData: Decodable {
+    let meals: [DessertMenu]
+}
+
+
+struct DessertMenu :Hashable, Decodable {
+    let strMealThumb : String
+    let strMeal : String
+}
+
+enum DRError : Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
 }
 
 #Preview {
     ContentView()
 }
+
