fb3813362d615a61ffe8a8c2c43385c04f8c2bd5
divya-prav <divya.praveen.5215@gmail.com>
2024-02-09 21:46:36 -0600

    View model Creadtion Done

---
+++ MyMeal/DetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailViewModel.swift
+//  MyMeal
+//
+//  Created by Divya Praveen on 2/7/24.
+//
+
+import Foundation
---
+++ MyMeal/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  MyMeal
+//
+//  Created by Divya Praveen on 2/7/24.
+//
+
+import Foundation
