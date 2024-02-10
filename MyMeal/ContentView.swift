cc5a3c2e2ded5676c07382b7b71dada2ba086c28
divya-prav <divya.praveen.5215@gmail.com>
2024-02-09 21:53:46 -0600

    Detail View Added

--- MyMeal/ContentView.swift
+++ MyMeal/ContentView.swift
@@ -7,58 +7,33 @@
 
 import SwiftUI
 
-
-class ViewModel: ObservableObject{
-    
-    @Published var recipes: [DessertMenu] = []
-    func fetch() {
-        guard let url = URL(string:
-            "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert")else{
-            return
-        }
-        
-        let task = URLSession.shared.dataTask(with: url){
-            data,_, error in
-            guard let data = data, error == nil else{
-                return
-            }
-            
-            do{
-                let responseData = try JSONDecoder().decode(ResponseData.self,from:
-                    data)
-                DispatchQueue.main.async {
-                    self.recipes = responseData.meals
-                }
-            }
-            catch{
-                print(error)
-            }
-        }
-        task.resume()
-    }
-}
-
 struct ContentView: View {
    
     @StateObject var viewModel = ViewModel()
     var body: some View {
         NavigationView {
-            List{
-                ForEach(viewModel.recipes,id: \.self){recipe in
-                    HStack {
-                        NavigationLink(destination : RecipeDetailView(recipe:recipe)){
-                            URLImage(urlString: recipe.strMealThumb)
-                                .frame(width: 100,height:100)
-                                .background(Color.gray)
-                            Text(recipe.strMeal)
-                                .bold()
-                            
+            ScrollView{
+                VStack(spacing:10){
+                    ForEach(viewModel.recipes,id: \.idMeal){recipe in
+                      NavigationLink(destination : RecipeDetailView(recipe:recipe)){
+                                HStack {
+                                URLImage(urlString: recipe.strMealThumb)
+                                    .frame(width: 100,height:100)
+                                    .background(Color.gray)
+                                Text(recipe.strMeal)
+                                    .bold()
+                                    .padding(.leading,10)
+                                    Spacer()
+                            }
+                           
                         }
-                       
-                    }
-                    .padding(10)
-                }
-                
+                        .padding(10)
+                   }
+                    
+               }
+               .padding(.horizontal)
+               .padding(.bottom, 20)
+               .frame(maxWidth: .infinity)
             }
             .navigationTitle("Dessert")
             .onAppear{
@@ -103,28 +78,95 @@ struct URLImage : View {
         task.resume()
     }
 }
-
 struct RecipeDetailView: View {
+    @StateObject var detailModel = DetailViewModel()
+    
     let recipe: DessertMenu
-
+    
     var body: some View {
-        VStack {
-            Text(recipe.strMeal)
-            // Add more details as needed
+        NavigationView {
+            VStack {
+                if detailModel.jsonData.isEmpty {
+                    Text("Loading...")
+                        .padding()
+                } else{
+                    
+                    
+                    List(detailModel.jsonData, id: \.idMeal) { detailMenu in
+                        VStack {
+                            AsyncImage(url:URL(string:detailMenu.strMealThumb )){
+                                image in image
+                                    .image?.resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .clipped()
+                            }
+                            Link(" For More Details!!",destination: URL(string: detailMenu.strSource)!)
+                                .font(.title3)
+                                .foregroundColor(.red)
+                                .underline()
+                                .padding(10)
+                            
+                            Link("Youtube Video",destination: URL(string: detailMenu.strYoutube)!)
+                                .font(.title3)
+                                .foregroundColor(.red)
+                                .underline()
+                                .padding(10)
+                            Text("Instructions: ").bold().underline().padding(10)
+                            Text(detailMenu.strInstructions)
+                            
+                            Text("Ingredients: ").bold().underline().padding(10)
+                            VStack(alignment: .leading) {
+                                ForEach(1...20, id: \.self) { i in
+                                    if let ingredient = getIngredient(i: i, detailMenu: detailMenu),
+                                       let measure = getMeasure(i: i, detailMenu: detailMenu),
+                                       !ingredient.isEmpty && !measure.isEmpty {
+                                        Text("\(ingredient)  :  \(measure)").padding(5)
+                                    }
+                                }
+                            }
+                            
+                        }
+                    }
+                    
+                    .listStyle(PlainListStyle())
+                    .padding(.horizontal)
+                    .padding(.bottom, 20)
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .navigationTitle(recipe.strMeal)
+            .onAppear {
+                detailModel.fetchData(withQuery: recipe.idMeal)
+            }
+           
         }
-        .navigationTitle(recipe.strMeal)
+       
+    }
+       
+    
+    private func getIngredient(i: Int, detailMenu: DetailMenu) -> String? {
+        let mirror = Mirror(reflecting: detailMenu)
+        let ingredient = mirror.children.first { $0.label == "strIngredient\(i)" }?.value as? String
+        return ingredient
+    }
+    
+    private func getMeasure(i: Int, detailMenu: DetailMenu) -> String? {
+        let mirror = Mirror(reflecting: detailMenu)
+        let measure = mirror.children.first { $0.label == "strMeasure\(i)" }?.value as? String
+        return measure
     }
 }
+
+
 
 struct ResponseData: Decodable {
     let meals: [DessertMenu]
 }
 
-
-struct DessertMenu :Hashable, Decodable {
-    let strMealThumb : String
-    let strMeal : String
+struct SingleDessert:Decodable{
+    let meals : [DetailMenu]
 }
+
 
 enum DRError : Error {
     case invalidURL
--- MyMeal/ViewModel.swift
+++ MyMeal/ViewModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+struct DessertMenu :Hashable, Decodable {
+    let idMeal :String
+    let strMealThumb : String
+    let strMeal : String
+}
+
+class ViewModel: ObservableObject{
+    
+    @Published var recipes: [DessertMenu] = []
+    func fetch() {
+        guard let url = URL(string:
+            "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert")else{
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url){
+            data,_, error in
+            guard let data = data, error == nil else{
+                return
+            }
+            
+            do{
+                let responseData = try JSONDecoder().decode(ResponseData.self,from:
+                    data)
+                DispatchQueue.main.async {
+                    self.recipes = responseData.meals
+                }
+            }
+            catch{
+                print(error)
+            }
+        }
+        task.resume()
+    }
+}
